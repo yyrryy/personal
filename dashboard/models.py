1de9b12b57d980604d4254d0f87abaed39dd9a07
yurey lenovo <you@example.com>
2026-06-09 21:55:23 +0100

    reer

--- dashboard/models.py
+++ dashboard/models.py
@@ -344,7 +344,7 @@ class Profile(models.Model):
         ('client', 'Client'),
     ]
     
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', default=None, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', default=None, null=True)
     user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, default='client')
     balance = models.FloatField(default=0.0)
     birthday = models.DateTimeField(null=True, blank=True)
